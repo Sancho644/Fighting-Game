8d866e5dcbb5a424f642e094cf41be77a64ef102
Alexandr <rfst@mail.ru>
2023-09-15 21:38:24 +0700

    Create initial scene, game bootsrpapper

---
+++ Assets/Scripts/Infrastructure/Game.cs
@@ -0,0 +1,16 @@
+﻿using Logic;
+using Services.Input;
+
+namespace Infrastructure
+{
+    public class Game
+    {
+        public static IInputService InputService;
+        public GameStateMachine StateMachine;
+
+        public Game(ICoroutineRunner coroutineRunner, LoadingCurtain curtain)
+        {
+            StateMachine = new GameStateMachine(new SceneLoader(coroutineRunner), curtain);
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,20 @@
+using Logic;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
+    {
+        [SerializeField] private LoadingCurtain _curtain;
+        
+        private Game _game;
+
+        private void Awake()
+        {
+            _game = new Game(this, _curtain);
+            _game.StateMachine.Enter<BootstrapState>();
+            
+            DontDestroyOnLoad(this);
+        }
+    }
+}
